b9c762a3281c7f3167621399e95e7e141db99022
Luze26 <yann.uzel@gmail.com>
2013-10-12 10:09:53 +0200

    A step forward

--- src/main/java/com/cathymini/cathymini2/services/ProductBean.java
+++ src/main/java/com/cathymini/cathymini2/services/ProductBean.java
@@ -6,25 +6,62 @@ package com.cathymini.cathymini2.services;
 
 import com.cathymini.cathymini2.model.Product;
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.faces.bean.ManagedBean;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 /**
  *
  * @author uzely
  */
 @ManagedBean(name="productBean")
+@Stateless
+@TransactionManagement(value=TransactionManagementType.CONTAINER)
 public class ProductBean {
     
-    @PersistenceContext(unitName="ProductService") private EntityManager manager;
+    @PersistenceContext(unitName="com.cathymini_CathyMini2_PU") private EntityManager manager;
     
     public void addProduct() {
         Product prod = new Product();
         prod.setName("test");
-        manager.persist(prod);   
+        UserTransaction tx;
+        try {
+            tx = (UserTransaction)new InitialContext().lookup("java:comp/UserTransaction");
+            tx.begin();
+            manager.persist(prod);   
+            tx.commit();
+        } catch (NamingException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NotSupportedException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SystemException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RollbackException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (HeuristicMixedException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (HeuristicRollbackException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalStateException ex) {
+            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public Collection<Product> getProducts() {
