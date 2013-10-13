20ff1fd250381bcc09bbaa373091836e9919cfd5
Luze26 <yann.uzel@gmail.com>
2013-10-13 10:31:24 +0200

    add admin template / change context path to '/' / use maven resourc eplugin to copy assets

--- src/main/java/com/cathymini/cathymini2/services/ProductBean.java
+++ src/main/java/com/cathymini/cathymini2/services/ProductBean.java
@@ -6,24 +6,16 @@ package com.cathymini.cathymini2.services;
 
 import com.cathymini.cathymini2.model.Product;
 import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.faces.bean.ManagedBean;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -36,7 +28,7 @@ public class ProductBean {
     
     @PersistenceContext(unitName="com.cathymini_CathyMini2_PU") private EntityManager manager;
     
-    private static final Logger logger = Logger.getLogger(ProductBean.class.getName());
+    private static final Logger logger = Logger.getLogger(ProductBean.class);
     
     public void addProduct() {
         Product prod = new Product();
@@ -48,22 +40,8 @@ public class ProductBean {
             manager.persist(prod);   
             tx.commit();
             logger.info("Product added");
-        } catch (NamingException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NotSupportedException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SystemException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (RollbackException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (HeuristicMixedException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (HeuristicRollbackException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SecurityException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalStateException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            logger.fatal("Erro while addind a product", ex);
         }
     }
     
