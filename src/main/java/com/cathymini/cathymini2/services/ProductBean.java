c1bf819c51fe52a89f93c2a1a197a2b60a833752
Luze26 <yann.uzel@gmail.com>
2013-10-13 23:42:53 +0200

    Delete useless css, change log propertie

--- src/main/java/com/cathymini/cathymini2/services/ProductBean.java
+++ src/main/java/com/cathymini/cathymini2/services/ProductBean.java
@@ -6,14 +6,17 @@ package com.cathymini.cathymini2.services;
 
 import com.cathymini.cathymini2.model.Product;
 import java.util.Collection;
+import java.util.Enumeration;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.UserTransaction;
 import org.apache.log4j.Logger;
 
@@ -31,6 +34,14 @@ public class ProductBean {
     private static final Logger logger = Logger.getLogger(ProductBean.class);
     
     public void addProduct() {
+        HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String param = request.getParameter("addProduct");
+        logger.debug(param);
+        Enumeration<String> elems = request.getParameterNames();
+        while(elems.hasMoreElements()) {
+            logger.debug(elems.nextElement());
+        }
+        
         Product prod = new Product();
         prod.setName("test");
         UserTransaction tx;
