72b2609a35d123fa84c9b16c7fc18d6b0d7a663c
Luze26 <yann.uzel@gmail.com>
2013-11-19 19:28:27 +0100

    merge the 2 projects + no more angular strap

--- CathyMini-js/app/js/modules/commonModule.js
+++
@@ -1,3 +0,0 @@
-angular.module('common', ['$strap.directives']);
-
-
---
+++ CathyMini2/src/main/webapp/assets/js/controllers/consumerCtrls.js
@@ -0,0 +1,120 @@
+ angular.module('common').
+  controller('consumerCtrl', ['$scope', '$http', function($scope, $http) {
+  
+    /** Initialize consumer form's attributes */
+    $scope.consumer = {};
+
+    /** Connected user info */
+    $scope.isConnected = {display: false, text: ""};
+    $scope.user = "";
+    
+    /** Failure feedback */
+    $scope.feedbackKo = {display: false, text: ""};
+    
+    /** Connection error on modals */
+    $scope.displayConnectionError = false;
+    
+    /**
+     * Connect a user
+     * @param {type} dismiss function for the modal
+     */
+    $scope.connect = function(dismiss) {
+        $scope.displayConnectionError = false;
+        $scope.feedbackKo.display = false;
+        
+        $http.post("http://localhost:8080//webresources/consumer/connect", $scope.consumer)
+            .success(function() { 
+                $scope.isConnected.display = true;  
+                $scope.isConnected.text = "You connect to CathyMini";
+                $scope.getCurrentUser();
+                dismiss();})
+            .error(function(errorMsg, status) { 
+                if (status === 400) {
+                    $scope.feedbackKo.display = true;
+                    $scope.feedbackKo.text = errorMsg;
+                } else {
+                    $scope.displayConnectionError = true;
+                }
+            });
+    };
+    
+    /**
+     * Logout a connected user
+     */
+    $scope.disconnect = function() {
+        $scope.displayConnectionError = false;
+        $scope.feedbackKo.display = false;
+        
+        $http.post("http://localhost:8080//webresources/consumer/logout", null)
+            .success(function() { 
+                $scope.isConnected.display = false;  
+                $scope.isConnected.text = "You logout";
+                $scope.getCurrentUser();
+            })
+            .error(function(errorMsg, status) { 
+                if (status === 400) {
+                    $scope.feedbackKo.display = true;
+                    $scope.feedbackKo.text = errorMsg;
+                } else {
+                    $scope.displayConnectionError = true;
+                }
+            });
+    };
+    
+    /**
+     * Get user session infos if the user is connected
+     */
+    $scope.getCurrentUser = function() {
+        $scope.displayConnectionError = false;
+        $scope.feedbackKo.display = false;
+        
+        $http.get("http://localhost:8080//webresources/consumer/seeCurrent", null)
+            .success(function(user) { 
+                console.log("user -> "+user);
+                if (user === "") {
+                    $scope.isConnected.display = false;  
+                    $scope.isConnected.text = "";
+                    $scope.user = "";
+                } else {
+                    $scope.isConnected.display = true;
+                    $scope.isConnected.text = "You are connected";
+                    $scope.user = user;
+                }
+            })
+            .error(function(errorMsg, status) { 
+                if (status === 400) {
+                    $scope.feedbackKo.display = true;
+                    $scope.feedbackKo.text = errorMsg;
+                } else {
+                    $scope.displayConnectionError = true;
+                }
+            });
+    };
+    
+    $scope.getCurrentUser();
+  }])
+    .controller('subscribeModalCtrl', ['$scope', '$http', function($scope, $http) {
+              /**
+     * Subscribe a new user
+     * @param {type} dismiss function for the modal
+     */
+    $scope.subscribe = function() {
+        $scope.displayConnectionError = false;
+        $scope.feedbackKo.display = false;
+        
+        $http.post("http://localhost:8080//webresources/consumer/suscribe", $scope.consumer)
+            .success(function() { 
+                $scope.isConnected.display = true;  
+                $scope.isConnected.text = "You correctly suscribe to CathyMini";
+                $scope.getCurrentUser();
+                dismiss();})
+            .error(function(errorMsg, status) { 
+                if (status === 400) {
+                    $scope.feedbackKo.display = true;
+                    $scope.feedbackKo.text = errorMsg;
+                } else {
+                    $scope.displayConnectionError = true;
+                }
+            });
+    };
+  }]);
