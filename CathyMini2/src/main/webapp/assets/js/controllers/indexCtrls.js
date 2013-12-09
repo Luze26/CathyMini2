deb6586a82b71eb168302112697d50010f72dcc0
Huguet <florent1802@hotmail.fr>
2013-12-09 07:18:16 +0100

    début de accueil

--- CathyMini2/src/main/webapp/assets/js/controllers/indexCtrls.js
+++ CathyMini2/src/main/webapp/assets/js/controllers/indexCtrls.js
@@ -1,24 +1,27 @@
 angular.module('index').
-        controller('indexCtrl', ['$scope', '$http',  function($scope) {
+        controller('indexCtrls', ['$scope', '$http', function($scope, $http) {
 
-        $scope.articles = [];
-        
-         $scope.loadArticles = function() {
-        if ($scope.loadMore) {
-          $http.post("/webresources/article/all", $scope.search)
-            .success(function(data) {
-              if (data.length < $scope.search.length) { //If there is no more product to load, end of the list
-                $scope.loadMore = false;
-              }
-
-              if ($scope.search.offset === 0) { //If it's a new search, we reset the list
                 $scope.articles = [];
-              }
 
-              $scope.search.offset += $scope.search.length; //Increment the offset
-              $scope.articles = $scope.articles.concat(data); //Add last loaded products
-            });
-        }
-      };
- 
-}]);    
+                /** If we continue to requests the server or not */
+                $scope.loadMore = true;
+
+                $scope.loadArticles = function() {
+                    console.log("ici");
+                    if ($scope.loadMore) {
+                        $http.post("/webresources/article/all", $scope.search)
+                                .success(function(data) {
+                                    if (data.length < $scope.search.length) { //If there is no more product to load, end of the list
+                                        $scope.loadMore = false;
+                                    }
+                                    if ($scope.search.offset === 0) { //If it's a new search, we reset the list
+                                        $scope.articles = [];
+                                    }
+
+                                    $scope.search.offset += $scope.search.length; //Increment the offset
+                                    $scope.articles = $scope.articles.concat(data); //Add last loaded products
+                                });
+                    }
+                };
+
+            }]);
