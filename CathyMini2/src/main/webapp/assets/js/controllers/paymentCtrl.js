35721ca15566e305f447e555a8092d1ffc5a39ca
Huguet <florent1802@hotmail.fr>
2013-12-11 17:03:32 +0100

    Revert "Mise a jour du Dossier de Conception Systeme/DocumentDeConception.odt"
    This reverts commit c54878570be50efe4d67bf30030ae836c3b66d97.

--- CathyMini2/src/main/webapp/assets/js/controllers/paymentCtrl.js
+++ CathyMini2/src/main/webapp/assets/js/controllers/paymentCtrl.js
@@ -1,63 +1,57 @@
 angular.module('payment').
     controller('paymentCtrl', ['$scope', '$http', 'cartService', function($scope, $http, cartService) {
     
-        // HTML Elements Ids
-        $scope.paymentTabId = ['#cart-payementTab', '#login-payementTab', '#address-payementTab', 
+        $scope.paymentTab = ['#cart-payementTab', '#login-payementTab', '#address-payementTab', 
             '#shipping-payementTab', '#payment-payementTab'];
-        $scope.paymentBtnId = ['#cart-payementBtn', '#login-payementBtn', '#address-payementBtn', 
-            '#shipping-payementBtn', '#payment-payementBtn'];
-        $scope.LastNextBtnId = ['#payment-lastBtn','#payment-nextBtn'];
         
-        // Initialize state variable
-        $scope.currentTab = 0;
-        $scope.NbElts = $scope.paymentBtnId.length;
+        $scope.paymentBtn = ['#cart-payementBtn', '#login-payementBtn', '#address-payementBtn', 
+            '#shipping-payementBtn', '#payment-payementBtn'];
     
         $scope.selectTab = function(select) {
-            if(!angular.element($scope.paymentBtnId[select]).hasClass('disabled')) {
+            if(!angular.element(select).hasClass('disabled')) {
                 $scope.activeTab(select);
             }
         };
         
         $scope.nextTab = function() {
-            if ($scope.currentTab !== $scope.NbElts-1) {
-                $scope.activeTab($scope.currentTab + 1);
+            var found = false;
+            for (var i = 1; i < $scope.paymentBtn.length ; i++) {
+                if(angular.element($scope.paymentBtn[i]).hasClass('disabled') && !found) {
+                    $scope.activeTab($scope.paymentBtn[i]);
+                    found = true;
+                }
             }
         };
         
         $scope.lastTab = function() {
-            if ($scope.currentTab !== 0) {
-                $scope.activeTab($scope.currentTab - 1);
-            }
+                var found = false;
+                for (var i = 2; i < $scope.paymentBtn.length ; i++) {
+                    if(angular.element($scope.paymentBtn[i]).hasClass('disabled') && !found) {
+                        $scope.activeTab($scope.paymentBtn[i-2]);
+                        found = true;
+                    }
+                }
+                
+                if (!found)
+                    $scope.activeTab($scope.paymentBtn[$scope.paymentBtn.length-2]);
         };
     
         $scope.activeTab = function(select) {
-            $scope.currentTab = select;
-            
-            for (var i = 0; i < $scope.NbElts ; i++) {
-                if (i < select) {
-                    angular.element($scope.paymentTabId[i]).addClass('hidden');
-                    angular.element($scope.paymentBtnId[i]).removeClass('disabled');
-                } else if (i === select) {
-                    angular.element($scope.paymentTabId[i]).removeClass('hidden');
-                    angular.element($scope.paymentBtnId[i]).removeClass('disabled');
+            var found = false;
+            for (var i = 0; i < $scope.paymentBtn.length ; i++) {
+                
+                if($scope.paymentBtn[i] === select) {
+                    angular.element($scope.paymentTab[i]).removeClass('hidden');
+                    angular.element($scope.paymentBtn[i]).removeClass('disabled');
+                    found = true;
+                } else if(!found) {
+                    angular.element($scope.paymentTab[i]).addClass('hidden');
+                    angular.element($scope.paymentBtn[i]).removeClass('disabled');
                 } else {
-                    angular.element($scope.paymentTabId[i]).addClass('hidden');
-                    angular.element($scope.paymentBtnId[i]).addClass('disabled');
+                    angular.element($scope.paymentTab[i]).addClass('hidden');
+                    angular.element($scope.paymentBtn[i]).addClass('disabled');
                 }
-            }
-            
-            if ($scope.currentTab === 0) {
-                angular.element($scope.LastNextBtnId[0]).addClass('disabled');
-                angular.element($scope.LastNextBtnId[1]).removeClass('disabled');
-            } else if ($scope.currentTab === $scope.NbElts-1) {
-                angular.element($scope.LastNextBtnId[0]).removeClass('disabled');
-                angular.element($scope.LastNextBtnId[1]).addClass('disabled');
-            } else {
-                angular.element($scope.LastNextBtnId[0]).removeClass('disabled');
-                angular.element($scope.LastNextBtnId[1]).removeClass('disabled');
             }
         };
 
-        // Initialize start HTML document properties
-        $scope.activeTab($scope.currentTab);
   }]);
